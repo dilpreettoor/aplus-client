e26447d86c534dc6f26d0c27924678dfdcf91f35
dilpreettoor <dilpreet.s.t@gmail.com>
2023-08-22 20:42:42 -0400

    first commit

---
+++ src/components/Header/Header/Header.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Header () {
+    return(
+        <>
+        <header className="headerNav">
+            <div className="headerNav__container">
+                
+            </div>
+        </header>
+        </>
+    )
+}
+
+export default Header;
