ec8f202de07439e9c0b9fe9c49031f96f035bdf5
dilpreettoor <dilpreet.s.t@gmail.com>
2023-08-26 15:02:30 -0400

    forgot to change branch, temp store in develop before switching branch

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -2,18 +2,19 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import redT from "../../assets/Images/redtshirt.jpg";
+import './ProductList.css';
 
 const ProductList = ({ ProductData, setProductData }) => {
   const navigate = useNavigate();
 
   return (
-    <section>
+    <section className="product">
       {ProductData.map((product) => (
-        <div className="product-card">
-          <div className="product-image">
-            <img src={product.imageURL} />
+        <div className="product__card">
+          <div className="product__card-item">
+            <img className="product__card-item--image" src={product.imageURL} alt={product.productName} />
           </div>
-          <div className="product-info">
+          <div className="product__card--info">
             <h2>{product.productName}</h2>
             <h3>{product.price}</h3>
           </div>
