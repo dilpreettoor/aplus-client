8d2b544b8d72d814c30f5b62ee967b3cb5020292
dilpreettoor <dilpreet.s.t@gmail.com>
2023-08-29 00:29:42 -0400

    added card layout to products, simulated adding to cart

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -2,21 +2,40 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import redT from "../../assets/Images/redtshirt.jpg";
-import './ProductList.css';
+import "./ProductList.css";
 
 const ProductList = ({ ProductData, setProductData }) => {
   const navigate = useNavigate();
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems([...cartItems, product]);
+    console.log(cartItems);
+  };
 
   return (
     <section className="product">
       {ProductData.map((product) => (
         <div className="product__card">
-          <div className="product__card-item">
-            <img className="product__card-item--image" src={product.imageURL} alt={product.productName} />
-          </div>
-          <div className="product__card--info">
-            <h2>{product.productName}</h2>
-            <h3>{product.price}</h3>
+          <div className="product__card-inner">
+            <div className="product__card-inner--front">
+              <div className="product__card-item">
+                <img
+                  className="product__card-item--image"
+                  src={product.imageURL}
+                  alt={product.productName}
+                />
+              </div>
+              <div className="product__card--info">
+                <h2>{product.productName}</h2>
+                <h3>{product.price}</h3>
+              </div>
+            </div>
+
+            <div className="product__card-inner--back">
+                <button onClick={ ()=> addToCart(product)}>Add to Cart</button>
+                <h2>Product Description</h2>
+            </div>
           </div>
         </div>
       ))}
