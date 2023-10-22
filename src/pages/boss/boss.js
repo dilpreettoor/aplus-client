5a738df7f4d6597e0acf379927608c3d28c853b3
dilpreettoor <dilpreet.s.t@gmail.com>
2023-10-22 16:55:42 -0400

    removed .env, causing errors

--- src/pages/boss/boss.js
+++ src/pages/boss/boss.js
@@ -2,10 +2,6 @@ import "./boss.css";
 
 const Boss = () => {
 
-    const adminEmail = process.env.ADMIN_EMAIL;
-    const googleClientId = process.env.GOOGLE_CLIENT_ID;
-    console.log(adminEmail);
-
   return (
     <section className="boss">
       
